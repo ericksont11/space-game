57dfda14fc7142b23acc95b7a07d68e54c5868a0
ericksont11 <ericksont91@gmail.com>
2019-02-18 18:30:00 -0500

    added outside and collapse functions

--- assets/javascript/cave.js
+++ assets/javascript/cave.js
@@ -4,14 +4,16 @@ var hello = -1
 var marginArray = []
 var rightCount = 48;
 var layers = 0
-var collapse = false;
+var collapse = "notTriggered";
 
+makeCave();
 
 function rocksFall() {
     $("#pop-up").css("display", "inline-block")
     $("p").html("That may have been a mistake...")
     setTimeout(function() {
         $("#pop-up").css("display", "none")
+        collapse = "true";
     }, 4000)
     var timer = 100;
     kels = 600
@@ -31,35 +33,36 @@ function rocksFall() {
 }
 
 $("#treasure").click(function(){
-    if (collapse === true) {
+    if (collapse === "waiting") {
         openTreasure()
         setTimeout(rocksFall, 1000);
     }
 })
 
 
-
-for (var a = 0; a < 9; a++) {
-    if (a < 9) {
-        marginArray = [-3,0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75,78,81,84,87,90,93,96]
+function makeCave() {
+    for (var a = 0; a < 9; a++) {
+        if (a < 9) {
+            marginArray = [-3,0,3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75,78,81,84,87,90,93,96]
+        }
+        else {
+        marginArray = [-3,0,3,6,9,12,15,18,21,24,69,72,75,78,81,84,87,90,93,96]
+        }
+        for (var d = 0; d < 34; d++){
+            var pictureArray = ["assets/images/rock.png", "assets/images/rock-two.png", "assets/images/rock-three.png", "assets/images/rock-four.png", "assets/images/rock-five.png"]
+            var rocks = $("<img>")
+            var number = Math.floor((Math.random() * marginArray.length))
+            rocks.attr("src", pictureArray[Math.floor((Math.random() * 5))])
+            rocks.addClass("falling-rocks")
+            $("#rock-div").append(rocks)
+            rocks.addClass("rockslide"+hello)
+            rocks.css("margin-left", marginArray[number]+"vw")
+            rocks.css("z-index", Math.floor((Math.random() * 3)))
+            rocks.css("margin-top", layers + "vh")
+            marginArray.splice(number, 1)
+        }
+        layers = layers + 5
     }
-    else {
-    marginArray = [-3,0,3,6,9,12,15,18,21,24,69,72,75,78,81,84,87,90,93,96]
-    }
-    for (var d = 0; d < 34; d++){
-        var pictureArray = ["assets/images/rock.png", "assets/images/rock-two.png", "assets/images/rock-three.png", "assets/images/rock-four.png", "assets/images/rock-five.png"]
-        var rocks = $("<img>")
-        var number = Math.floor((Math.random() * marginArray.length))
-        rocks.attr("src", pictureArray[Math.floor((Math.random() * 5))])
-        rocks.addClass("falling-rocks")
-        $("body").append(rocks)
-        rocks.addClass("rockslide"+hello)
-        rocks.css("margin-left", marginArray[number]+"vw")
-        rocks.css("z-index", Math.floor((Math.random() * 3)))
-        rocks.css("margin-top", layers + "vh")
-        marginArray.splice(number, 1)
-    }
-    layers = layers + 5
 }
 
 
@@ -69,7 +72,7 @@ function makeRocks () {
     var number = Math.floor((Math.random() * marginArray.length))
     rocks.attr("src", pictureArray[Math.floor((Math.random() * 5))])
     rocks.addClass("falling-rocks")
-    $("body").append(rocks)
+    $("#rock-div").append(rocks)
     rocks.addClass("rockslide"+hello)
     rocks.css("margin-left", marginArray[number]+"vw")
     rocks.css("z-index", Math.floor((Math.random() * 3)+3))
@@ -83,24 +86,56 @@ function openTreasure () {
 
 $(document).keydown(function( event ) {
     if ( event.which == 37 || event.which == 65) {
-        if (rightCount > 10 && collapse === false) {
+        if (rightCount > 13 && (collapse === "notTriggered" || collapse === "true")) {
             event.preventDefault();
             rightCount = rightCount - 0.5
-            $("#character").css('margin-left', rightCount+"vw");
+            $("#character").css('left', rightCount + "%");
             console.log(rightCount)
+        }
+        else if  (collapse === "notInCave") {
+            event.preventDefault();
+            rightCount = rightCount - 0.5
+            $("#character").css('left', rightCount + "%");
+            console.log(rightCount)
+        }
+        if (rightCount < 3 && collapse === "notInCave") {
+            $("#character").css("left", "98%")
+            $("#background").removeClass("outside-floor")
+            $("#background").addClass("cave-floor")
+            $("#background").attr("src", "assets/images/cave-floor.png")
+            $("#rock-div").empty()
+            $("#treasure").css("display", "inline-block")
+            collapse = "notTriggered"
+            rightCount = 98
+            layers = 0
+            makeCave()
+        }
+        if (rightCount < 15 && collapse === "notTriggered") {
+            collapse = "waiting"
+            $("#pop-up").css("display", "inline-block")
         }
     }
     if ( event.which == 39 || event.which == 68 ) {
-        if (rightCount < 90 && collapse === false) {
+        if (rightCount < 90 && (collapse === "notTriggered" || collapse === "notInCave")) {
             event.preventDefault();
             rightCount = rightCount + 0.5
-            $("#character").css('margin-left', rightCount+"vw");
-            console.log(rightCount)
+            $("#character").css('left', rightCount + "%");
         }
-    }
-    if (rightCount < 15 && collapse === false) {
-        collapse = true
-        $("#pop-up").css("display", "inline-block")
+        else if (rightCount < 60 && collapse === "true") {
+            event.preventDefault();
+            rightCount = rightCount + 0.5
+            $("#character").css('left', rightCount + "%");
+        }
+        else if (rightCount > 85 && collapse === "notTriggered") {
+            $("#background").removeClass("cave-floor")
+            $("#background").addClass("outside-floor")
+            $("#background").attr("src", "assets/images/outside-floor.png")
+            $("#rock-div").empty()
+            $("#character").css("left", "2%")
+            $("#treasure").css("display", "none")
+            collapse = "notInCave"
+            rightCount = 2
+        }
     }
     
 
